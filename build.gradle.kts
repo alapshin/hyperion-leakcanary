25215571e1eeae5a471d3592156bb11bd8e4e5e4
Andrei Lapshin <alapshin@fastmail.com>
2019-10-23 16:03:10 +0300

    Remove unused compilation options

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,17 +8,3 @@ allprojects {
         gradlePluginPortal()
     }
 }
-
-subprojects {
-    project.afterEvaluate {
-        project.tasks.withType(JavaCompile::class).configureEach {
-            // Show more compilation errors
-            // By default javac outputs 100 errors at most
-            // When using multiple libraries that rely on annotation processing
-            // this limit could be easily exceeded and real cause can be swallowed
-            // See https://github.com/google/dagger/issues/306#issuecomment-180283287
-            options.compilerArgs.addAll(listOf("-Xmaxerrs", "500"))
-        }
-    }
-}
-
